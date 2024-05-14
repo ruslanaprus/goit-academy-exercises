22e8453b984d1d9c69fa361a8f8cf37e849e7438
Slpixe <slpixe@gmail.com>
2024-05-14 14:22:50 +0100

    feat: add junit and triangle test

--- HomeworkEight/src/main/java/org/example/shapes/Triangle.java
+++ HomeworkEight/src/main/java/org/example/shapes/Triangle.java
@@ -6,10 +6,10 @@ import java.util.Arrays;
 
 public class Triangle extends Shape {
     private final String name = "Triangle";
-    private Point[] trianglePoints = new Point[3];
-    private Point p1;
-    private Point p2;
-    private Point p3;
+    private final Point[] trianglePoints = new Point[3];
+    private final Point p1;
+    private final Point p2;
+    private final Point p3;
 
     public Triangle(int[] coordinates) {
         if (coordinates.length == 0) {
@@ -75,5 +75,35 @@ public class Triangle extends Shape {
 
         System.out.println("Triangle sides: [POINT1 - POINT2] = " + String.format("%.2f", c) + ", [POINT2 - POINT3] = " + String.format("%.2f", a) + ", [POINT1 - POINT3] = " + String.format("%.2f", b));
 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        Point[] bob = (Point[]) o;
+        System.out.println("What is trianglePoints" + Arrays.toString(trianglePoints));
+        System.out.println("What is bob" + Arrays.toString(bob));
+        return
+                trianglePoints[0].x == bob[0].x
+                        && trianglePoints[0].y == bob[0].y
+                        && trianglePoints[1].x == bob[1].x
+                        && trianglePoints[1].y == bob[1].y
+                        && trianglePoints[2].x == bob[2].x
+                        && trianglePoints[2].y == bob[2].y
+                ;
+    }
+
+    public static boolean arePointsTheSame(Triangle tri, Point[] points) {
+        return tri.trianglePoints[0].x == points[0].x &&
+                tri.trianglePoints[0].y == points[0].y &&
+                tri.trianglePoints[1].x == points[1].x &&
+                tri.trianglePoints[1].y == points[1].y &&
+                tri.trianglePoints[2].x == points[2].x &&
+                tri.trianglePoints[2].y == points[2].y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(trianglePoints);
     }
 }
